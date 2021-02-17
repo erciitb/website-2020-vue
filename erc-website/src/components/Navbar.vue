b73ce7026a7a319c1e9ea72f0f01577fff849f61
Shubham Ojha <63144588+CommanderBaman@users.noreply.github.com>
2021-02-17 14:23:08 +0530

    Router Configuration + other small changes (#2)
    * add pull request template + router config + fonts + reformat pages
    * add changes
    * fix favicon

--- erc-website/src/components/Navbar.vue
+++ erc-website/src/components/Navbar.vue
@@ -1,13 +1,29 @@
 <template>
-  <div id="Navbar">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="navbar">
+    <!-- render out all links from router -->
+    <div class="router-links" v-for="route in routes" :key="route.id">
+      <router-link :to="route.link">{{ route.displayName }}</router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import { routeOptions } from '@/utils/constants'
+
 export default {
-  name: 'Navbar'
+  name: 'Navbar',
+  setup() {
+    // creating links from the router
+    const routes = routeOptions.map((route, index) => {
+      return {
+        link: route.path,
+        displayName: route.displayName,
+        id: index
+      }
+    })
+
+    return { routes }
+  }
 }
 </script>
 
--- erc-website/src/utils/constants.js
+++ erc-website/src/utils/constants.js
@@ -1,1 +1,39 @@
 // export constants from here -> like categories, event names, navbar names and their respective links
+
+// for the router
+export const routeOptions = [
+  {
+    path: '/',
+    displayName: 'Home',
+    componentFolder: 'Home',
+    componentName: 'Home'
+  },
+  {
+    path: '/events',
+    displayName: 'Events',
+    componentFolder: 'Events',
+    componentName: 'EventsMain'
+  },
+  {
+    path: '/projects',
+    displayName: 'Projects',
+    componentFolder: 'Projects',
+    componentName: 'ProjectsMain'
+  },
+  {
+    path: '/team-details',
+    displayName: 'Team Details',
+    componentFolder: 'Team Details',
+    componentName: 'TeamDetails'
+  },
+  // redirects
+  {
+    path: '/home',
+    redirect: '/'
+  },
+  // if link not found -> redirecting to home
+  {
+    path: '/:catchAll(.*)',
+    redirect: '/'
+  }
+]
