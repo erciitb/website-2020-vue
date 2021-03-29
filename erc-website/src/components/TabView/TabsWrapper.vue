9680f70e433d47d65ce29f48c47568ed2dbe51b1
CommanderBaman <63144588+CommanderBaman@users.noreply.github.com>
2021-03-29 17:54:33 +0530

    finish vertical tabs + add passing styles example in event page

--- erc-website/src/components/TabView/TabsWrapper.vue
+++ erc-website/src/components/TabView/TabsWrapper.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="tabsWrapper" class="tabsContainerCustomStylesFromParent">
+  <div
+    :class="{
+      tabsWrapper: true,
+      tabsContainerCustomStylesFromParent: true,
+      tabsWrapperVertical: isVertical
+    }"
+  >
     <div class="tabHeadingWrapper" ref="headingContainer">
       <ul
         class="tabHeadingList tabHeadingCustomStylesFromParent"
@@ -21,6 +27,7 @@
       </ul>
     </div>
     <div class="displayTabWrapper tabDisplayCustomStylesFromParent">
+      <!-- @slot Content of the tab -->
       <slot></slot>
     </div>
   </div>
@@ -30,13 +37,15 @@
 import { ref } from '@vue/runtime-core'
 
 /**
- * TabsWrapper Component
+ * @displayName TabsWrapper
  * Used to contain tabs from Tab component
  *
  * This component will resize itself to fit into the container it is placed in
- * Each tab name has a minimum of 6vw width, heading will be displayed in a single line only
+ * Each tab name has a minimum of 6vw width, heading will be displayed in a single line only (container resizes to fit name)
  * Font sizes will be overwritten for the tab names and tab content, use !important property to overwrite
  * Backgrounds are not opaque, but blur effect has been applied => if something is behind it, it will be blurred
+ * If vertical property is true, the tabs would be vertical. The headings will cover from 17% (desktop) to 30% (mobile) of width of the container
+ * If vertical, the tabs have a height between 50px and 85px (converted to vh)
  *
  * Can inherit styles from parent via these classes (if defined via ::v-deep)
  *  - tabsContainerCustomStylesFromParent (for full container),
@@ -50,9 +59,34 @@ import { ref } from '@vue/runtime-core'
 export default {
   name: 'TabsWrapper',
   props: {
-    headings: Array,
-    selectedIndex: Number,
-    changeIndex: Function
+    /**
+     * Contains headings of the tabs
+     */
+    headings: {
+      type: Array,
+      required: true
+    },
+    /**
+     * Index of tab that is selected
+     */
+    selectedIndex: {
+      type: Number,
+      required: true
+    },
+    /**
+     * Function that is called for changing the index
+     */
+    changeIndex: {
+      type: Function,
+      required: true
+    },
+    /**
+     * Boolean value that determines if the tabs are vertically displayed or not
+     */
+    isVertical: {
+      type: Boolean,
+      default: false
+    }
   },
   setup() {
     const headingContainer = ref(null)
